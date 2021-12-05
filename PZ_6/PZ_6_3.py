bb0dd2eeb64347196019d3a4895ac58d21f6f3a8
ERROR15515 <90280871+ERROR15515@users.noreply.github.com>
2021-12-05 18:40:24 +0300

    Update PZ_6_3.py

--- PZ_6/PZ_6_3.py
+++ PZ_6/PZ_6_3.py
@@ -1,17 +1,17 @@
 # Дан список размера N. Заменить каждый элемент списка
 # на среднее арифметическое этого элемента и его соседей.
 
-from random import randint
+from random import randint  # Импортирогвание "random"
 
 
-n = int(input('Введите размер списка:'))
+n = int(input('Введите размер списка:'))    # Ввод переменных
 lst = [randint(0, 50) for i in range(n)]
 lstt = []
 
 n -= 1
 print(lst)
 
-hlp = round((lst[1] + lst[0]) / 2, 1)
+hlp = round((lst[1] + lst[0]) / 2, 1)   # Расчет первых двух элементов
 lstt.insert(0, hlp)
 
 for i in range(n+1):
@@ -19,6 +19,7 @@ for i in range(n+1):
         hlp = round((lst[i]+lst[i-1]+lst[i+1])/3, 1)
         lstt.insert(i+1, hlp)
 
-hlp = round((lst[n-1] + lst[n]) / 2, 1)
+hlp = round((lst[n-1] + lst[n]) / 2, 1)   # Расчет последних двух элементов
 lstt.insert(n, hlp)
+
 print(lstt)
