510ce9e32429fac97fa28d9be3906faa4f3ff70d
ERROR15515 <gavrish.mitia2017@yandex.ru>
2021-12-05 17:33:16 +0300

    added PZ_6

---
+++ PZ_6/PZ_6_2.py
@@ -0,0 +1,22 @@
+# Дан список размера N. Найти количество участков, на которых его элементы
+# монотонно возрастают.
+
+import random
+
+n = input("Введите размер списка: ")
+while type(n) != int:
+    try:
+        n = int(n)
+    except ValueError:
+        print("Введено неверное значение.")
+        n = input("Введите размер списка: ")
+a = [random.randint(0, 20) for i in range(n)]
+print(a)
+t = 0
+for i in range(n):
+    if a[i] < a[i+1]:
+        t += 1
+    if n - i == 2:
+        break
+
+print(t)
---
+++ PZ_6/PZ_6_3.py
@@ -0,0 +1,24 @@
+# Дан список размера N. Заменить каждый элемент списка
+# на среднее арифметическое этого элемента и его соседей.
+
+from random import randint
+
+
+n = int(input('Введите размер списка:'))
+lst = [randint(0, 50) for i in range(n)]
+lstt = []
+
+n -= 1
+print(lst)
+
+hlp = round((lst[1] + lst[0]) / 2, 1)
+lstt.insert(0, hlp)
+
+for i in range(n+1):
+    if i < n and i != 0:
+        hlp = round((lst[i]+lst[i-1]+lst[i+1])/3, 1)
+        lstt.insert(i+1, hlp)
+
+hlp = round((lst[n-1] + lst[n]) / 2, 1)
+lstt.insert(n, hlp)
+print(lstt)
